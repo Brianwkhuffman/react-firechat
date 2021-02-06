547b8b594ef2ea873b4d368977a71f6a7abe8d1a
Brianwkhuffman <brianwkhuffman@gmail.com>
2021-02-05 17:50:48 -1000

    Created firebase project and linked up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyB7wUeA1RsnI4Hi_bGV5c39MZuFAXVUpL0",
+    authDomain: "react-firechat-1bb49.firebaseapp.com",
+    projectId: "react-firechat-1bb49",
+    storageBucket: "react-firechat-1bb49.appspot.com",
+    messagingSenderId: "762191356819",
+    appId: "1:762191356819:web:5e91bc16d4b6414b96fe66"
+});
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello World!</h1>
     </div>
   );
